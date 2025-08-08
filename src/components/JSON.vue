624b38585c8ab81a6332a2062b5d40dd57ff77b9
Disha Kheni <dkhe0003@student.monash.edu>
2025-08-08 20:33:42 +1000

    week-2 Activity-3.1 Render a list containing author names and their birth years

--- src/components/JSON.vue
+++ src/components/JSON.vue
@@ -11,6 +11,11 @@
       <h3>Iterating through Arrays</h3>
       <!-- Activity 6: Render a list containing author names and their birth years. Hint: Make use of the v-for directive to iterate through the array of authors. -->
       <!-- TODO: CODE TO RENDER LIST OF AUTHORS HERE -->
+      <ul>
+        <li v-for="author in authors" :key="author.id">
+          {{ author.name }} ({{ author.birthYear }})
+        </li>
+      </ul>
 
       <h3>Filtering Arrays</h3>
       <!-- Activity 7: Render a list containing authors born after 1850. Hint: Make use of the v-for directive to iterate through the array of authors that you have filtered out. -->
