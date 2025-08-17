2907d93cba1fcd6e7e7f918ff9eabb21061b4ab5
Disha Kheni <dkhe0003@student.monash.edu>
2025-08-17 17:23:50 +1000

    week-3 Activity 6: Display User Information in a Card via Submit Button

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -46,7 +46,7 @@
               class="form-control"
               id="reason"
               rows="3"
-              v-model="farmData.reason"
+              v-model="formData.reason"
             ></textarea>
           </div>
           <div class="text-center">
@@ -56,13 +56,63 @@
         </form>
       </div>
     </div>
+    <div class="row mt-5" v-if="submittedCards.length">
+      <div class="d-flex flex-wrap justify-content-start">
+        <div
+          v-for="(card, index) in submittedCards"
+          :key="index"
+          class="card m-2"
+          style="width: 18rem"
+        >
+          <div class="card-header">User Information</div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Username: {{ card.username }}</li>
+            <li class="list-group-item">Password: {{ card.password }}</li>
+            <li class="list-group-item">
+              Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
+            </li>
+            <li class="list-group-item">Gender: {{ card.gender }}</li>
+            <li class="list-group-item">Reason: {{ card.reason }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-// Our logic will go here
+import { ref } from 'vue'
+
+const formData = ref({
+  username: '',
+  password: '',
+  isAustralian: false,
+  reason: '',
+  gender: '',
+})
+
+const submittedCards = ref([])
+
+const submitForm = () => {
+  submittedCards.value.push({
+    ...formData.value,
+  })
+}
 </script>
 
 <style scoped>
-/* Our logic will go here */
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.card-header {
+  background-color: #275fda;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+.list-group-item {
+  padding: 10px;
+}
 </style>
