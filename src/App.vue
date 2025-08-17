06169d64b995bf326712b03c8bb39d6dffcff61f
Disha Kheni <dkhe0003@student.monash.edu>
2025-08-17 16:21:02 +1000

    Week-3 Activity 1: Create Form Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup>
+import Form from './components/Form.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import JSON from './components/JSON.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <JSON />
-    </div>
-  </header>
+    <div class="wrapper"> -->
+  <!-- <HelloWorld msg="You did it!" />
+      <JSON /> -->
+  <Form />
+  <!-- </div>
+  </header> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- <main> -->
+  <!-- <TheWelcome /> -->
+  <!-- </main> -->
 </template>
 
 <style scoped>
