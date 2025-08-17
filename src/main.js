dd90546b619580702364792159bc28eda332542a
Disha Kheni <dkhe0003@student.monash.edu>
2025-08-17 16:42:21 +1000

    week-3 Activity 4: Install and Set up Bootstrap 5

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import './assets/main.css'
-// Import Bootstrap's CSS
+
+//import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-// Import Bootstrap's JavaScript (includes Popper)
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-import './style.css'
+// import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
