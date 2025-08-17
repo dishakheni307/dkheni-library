84594b9c7c66909200ee99fd7eb3c96802c84d37
Disha Kheni <dkhe0003@student.monash.edu>
2025-08-17 16:37:26 +1000

    week-3 Activity 3: Form styling with CSS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import './assets/main.css'
+// Import Bootstrap's CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Import Bootstrap's JavaScript (includes Popper)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
